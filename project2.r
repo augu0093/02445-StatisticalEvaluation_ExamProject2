9e0c96d3ceec54382ad4689e32ad0cdd040a8938
August Andersen <augu0093@gmail.com>
2020-01-13 11:54:02 +0100

    Main file

---
+++ project2.r
@@ -0,0 +1,15 @@
+# Author: August Semrau Andersen - s183918
+
+# Set working directory
+setwd("C:/Users/August/Dropbox/3semester/Projekt_i_statistisk_evaluering/Statistical_Evaluation_Project2")
+
+
+
+##########################################
+# Start of project 2.
+##########################################
+
+
+# Load data
+load("fosfor_data.RData")
+
